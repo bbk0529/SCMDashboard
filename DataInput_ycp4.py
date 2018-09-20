37c26aca744e9ad0e9979d89eadbfbfed863fa6d
bbk0529 <bbk0529@gmail.com>
2018-09-20 11:27:48 +0900

    type

--- DataInput_ycp4.py
+++ DataInput_ycp4.py
@@ -1,62 +1,62 @@
-def ycp4Update() :
-    import pandas as pd
-    import time
-    import datetime
+
+import pandas as pd
+import time
+import datetime
 
 
-    starttime=time.time()
-    #filename='F-KR-YMON_180813.XLSX'
-    errorfile=open('errorfile.txt','a')
-    filename="Q:\\KRGrp007\\★NSC Meeting\\현황판\\YCP4.XLSX"
-    sheet_name='ycp4'
-    print("YMON file to be read")
-    df=pd.read_excel(filename, header=0)
-    print("YCP4 file loaded")
-    #df=pd.read_excel(filename, sheet_name=sheet_name, header=1)
-    #df=df.loc[:,['Material',]]
-    df=df.loc[:,[[
-        'Material','Material Description','Average consumption of 3 months',
-        'Average consumption of 12 months','MRP Unrestr. stock',
-        'Quantity delayed purch.orders','Quantity current purch.orders',
-        'Total Reservierungen / Sales Orders',9]]]
-    df.columns=['Material', 'Description', 'Con3M','Con12M','Stock','DelayedPO','Incoming','Order', 'MRP Type']
-    df['Status']= df['Stock'] + df['Order'] + df['Incoming'] + df['DelayedPO']
+starttime=time.time()
+#filename='F-KR-YMON_180813.XLSX'
+errorfile=open('errorfile.txt','a')
+filename="Q:\\KRGrp007\\★NSC Meeting\\현황판\\YCP4.XLSX"
+sheet_name='ycp4'
+print("YMON file to be read")
+df=pd.read_excel(filename, header=0)
+print("YCP4 file loaded")
+#df=pd.read_excel(filename, sheet_name=sheet_name, header=1)
+#df=df.loc[:,['Material',]]
+df=df.loc[:,[
+    'Material','Material Description','Average consumption of 3 months',
+    'Average consumption of 12 months','MRP Unrestr. stock',
+    'Quantity delayed purch.orders','Quantity current purch.orders',
+    'Total Reservierungen / Sales Orders', 'MRP Type']]
+df.columns=['Material', 'Description', 'Con3M','Con12M','Stock','DelayedPO','Incoming','Order', 'MRP Type']
+df['Status']= df['Stock'] + df['Order'] + df['Incoming'] + df['DelayedPO']
 
-    #df=df.set_index('Pn')
+#df=df.set_index('Pn')
 
-    from loteam.models import YCP4
-    i=0
-    for i,v in df.iterrows():
-        i=i+1
-        if (i%100 = 0) : print(i,"inputted")
-        try :
-            YCP4.objects.update_or_create(
-                Material = v['Material'],
-                defaults={
-                    'Description':v['Description'],
-                    'Con3M':v['Con3M'],
-                    'Con12M':v['Con12M'],
-                    'Stock':v['Stock'],
-                    'Incoming':v['Incoming'] + v['DelayedPO'],
-                    'Order': v['Order'],
-                    'Status': v['Status'],
-                    'Mrp': v['MRP Type'],
-                }
-            )
-
-
-
-        except Exception as ex :
-            print(ex, v)
-            errorfile.write((str(datetime.datetime.now())+ str(ex) + v['Material'] ))
-
-            continue
+from loteam.models import YCP4
+i=0
+for i,v in df.iterrows():
+    i=i+1
+    if (i%100 = 0) : print(i,"inputted")
+    try :
+        YCP4.objects.update_or_create(
+            Material = v['Material'],
+            defaults={
+                'Description':v['Description'],
+                'Con3M':v['Con3M'],
+                'Con12M':v['Con12M'],
+                'Stock':v['Stock'],
+                'Incoming':v['Incoming'] + v['DelayedPO'],
+                'Order': v['Order'],
+                'Status': v['Status'],
+                'Mrp': v['MRP Type'],
+            }
+        )
 
 
 
-    errorfile.close()
+    except Exception as ex :
+        print(ex, v)
+        errorfile.write((str(datetime.datetime.now())+ str(ex) + v['Material'] ))
+
+        continue
 
 
-    print("completed")
-    print(time.time()-starttime)
-    print("completed")
+
+errorfile.close()
+
+
+print("completed")
+print(time.time()-starttime)
+print("completed")
